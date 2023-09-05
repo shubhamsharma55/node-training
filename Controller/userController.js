84dcd91bb3bad03f38d8f461f339f3ab9d1176fa
Shubham Sharma <skssharmashivam@gmail.com>
2023-09-05 12:00:37 +0530

    changes

--- Controller/userController.js
+++ Controller/userController.js
@@ -46,26 +46,59 @@ exports.userRegister = async (req, res, next) => {
 
 
 
+// exports.getUsers = async (req, res, next) => {
+//   try {
+//       const users = await UserModel.findAll({
+//         attributes: ["name", "username", "email"],
+//       });
+//       if (users) {
+//         return res.status(201).json({
+//           message: "success",
+//           payload: {
+//             data: users,
+//           },
+//         });
+//       } else {
+//         throw new Error("User not found");
+//       }
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "failed",
+//       payload: {
+//         data: error,
+//       },
+//     });
+//   }
+// };
+
 exports.getUsers = async (req, res, next) => {
   try {
-      const users = await UserModel.findAll({
-        attributes: ["name", "username", "email"],
-      });
-      if (users) {
-        return res.status(201).json({
-          message: "success",
-          payload: {
-            data: users,
-          },
-        });
-      } else {
-        throw new Error("User not found");
-      }
+
+    const queryString = req.query;
+    const permitedQuery = ['limit', 'order'];
+    const filteredQueryString = Object.fromEntries(
+      Object.entries(queryString).filter(([key]) => permitedQuery.includes(key))
+    );
+    console.log(filteredQueryString);
+
+    const users = await UserModel.findAll({
+      attributes: ["name", "username", "email"],
+      limit: 10,
+      // order: []
+    });
+
+
+    return res.status(200).json({
+      message: "success",
+      payload: {
+        data: users,
+      },
+    });
   } catch (error) {
     res.status(500).json({
       message: "failed",
       payload: {
-        data: error,
+        error: error.message,
       },
     });
   }
